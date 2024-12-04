7db06511fb20792850a2b42b1bb3763bfe1443dd
Barnold88 <bandslover@outlook.com>
2024-12-04 22:47:51 +0000

    Add a lot of helper code to help automate temporary environments for testing

--- app/src/file_test.go
+++ app/src/file_test.go
@@ -1,34 +1,72 @@
 package main
 
 import (
+	"fmt"
+	"io/fs"
 	"os"
 	"testing"
 )
 
-func writeTestFiles() bool {
+var FILEPERM fs.FileMode = 0600
 
-	var errors []error
-
-	errors = append(errors, os.WriteFile("test.txt", []byte("hello world"), 0644))
-	errors = append(errors, os.WriteFile("main.go", []byte("package main\n\nfunc main(){\n fmt.Println(\" Hello World \")"), 0644))
-
-	for _, err := range errors {
-
-		if err != nil {
-			return false
-		}
+func directoryExists(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false, nil
 	}
-	return true
+	if err != nil {
+		return false, err
+	}
+	return info.IsDir(), nil
 }
 
-func cleanTestFiles() bool {
+func writeTestFiles(path string) error {
+
+	// Attempt to generate TestFiles
+	err := os.WriteFile(path+"/main.go", []byte("package main\n\nfunc main(){\n\n\tfmt.Println(\"Hello World\")\n\n}"), FILEPERM)
+	// Attempt to generate TestFiles
+
+	return err
+}
+
+func writeTestDirectory(path string) error {
+
+	exists, err := directoryExists(path)
+
+	if err != nil {
+		return fmt.Errorf("Error - writeTestDirectory: An error was caught in directoryExists %s\n", err)
+	}
+	if exists == true {
+		return fmt.Errorf("Error - writeTestDirectory: Tried writing test directory (%s), but it already exists\n", path)
+	} else {
+		possibleError := os.Mkdir(path, FILEPERM)
+		if possibleError != nil {
+			return possibleError
+		}
+	}
+
+	return nil
+}
+
+func cleanup(path string) bool {
 
 	return false
 }
 
-func TestreadFileToLines(t *testing.T) {
+func TestReadFileToLines(t *testing.T) {
 
-	writeTestFiles() // Make the temp files
+	var errors []error
+
+	errors = append(errors, writeTestDirectory("test"))
+	errors = append(errors, writeTestFile("test", "")) // Make the temp files
+
+	for _, err := range errors {
+
+		if err != nil {
+			t.Errorf("Error - TestReadFileToLines: %s | ", err)
+		}
+
+	}
 
 	// file, err := os.ReadFile()
 

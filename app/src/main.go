a797a6f4ba0beff73db12d9b077659a40087632b
Barnold88 <bandslover@outlook.com>
2024-12-06 15:37:18 +0000

    Fix main.go

--- app/src/main.go
+++ app/src/main.go
@@ -1,23 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	files, err := seekGoFiles(".")
-
-	if err == nil {
-
-		for _, file := range files {
-
-			fmt.Println(file)
-
-		}
-
-	} else {
-		fmt.Printf("Error: %s\n", err)
-	}
-
 }
-
-//

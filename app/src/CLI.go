b499a0cc07e62b681ff45416d7105a8b3d1ebb5a
Barnold88 <bandslover@outlook.com>
2024-12-17 01:50:21 +0000

    Fully work parseArgs to return custom struct holding relevant path and any flag I need

--- app/src/CLI.go
+++ app/src/CLI.go
@@ -3,32 +3,46 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
-func parseArgs(args []string) {
-	re := regexp.MustCompile(`^(--\w+(-\w+)*=?|\-\w)(.*)?$`)
+type cliArgs struct {
+	seekPath string
+	flags    map[string]bool
+}
+
+func defaultCLIargs() cliArgs {
+	return cliArgs{
+		flags: make(map[string]bool),
+	}
+}
+
+func parseArgs(args []string) cliArgs {
+
+	cliArgs := defaultCLIargs()
+	re := regexp.MustCompile(`^--([a-zA-Z0-9-_]+)(=(.*))?$`)
 
 	for i := 0; i < len(args); i++ {
 		arg := args[i]
-		if matches := re.FindStringSubmatch(arg); matches != nil {
-			flag := matches[1]
 
-			switch flag {
-			case "--allow":
-				// allow = true
-			case "--verbose":
-				// verbose = true
-			case "--name":
-				// Handle flags with a value (e.g., --name=value or --name value)
-				if i+1 < len(args) && args[i+1][0] != '-' {
-					// name = args[i+1]
-					i++ // Skip the next argument
+		if matches := re.FindStringSubmatch(arg); matches != nil {
+			flagName := matches[1]
+			flagValue := matches[3]
+
+			switch flagName {
+			case "name":
+				if flagValue != "" {
+					cliArgs.seekPath = flagValue
+				} else if i+1 < len(args) && !strings.HasPrefix(args[i+1], "-") {
+					cliArgs.seekPath = args[i+1]
+					i++
 				} else {
 					fmt.Println("Error: --name requires a value")
 				}
 			default:
-				fmt.Printf("Unknown flag: %s\n", flag)
+				cliArgs.flags[flagName] = true
 			}
 		}
 	}
+	return cliArgs
 }

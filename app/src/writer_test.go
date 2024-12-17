e808642d20e86f0e575d6b6165f72bffe9513ce5
Barnold88 <bandslover@outlook.com>
2024-12-17 14:46:10 +0000

    Complete TestCapitalizeFunctionName

--- app/src/writer_test.go
+++ app/src/writer_test.go
@@ -132,14 +132,63 @@ func TestCapitalizeFunctionName(t *testing.T) {
 		input    string
 		expected string
 	}{
-		{"Test 1", "input", "output"},
+		{"Test 1", "someFunction", "SomeFunction"},
+		{"Test 2", "QuiteliteralOtherfunction", "QuiteliteralOtherfunction"},
+		{"Test 3", "anotherExampleFunction", "AnotherExampleFunction"},
+		{"Test 4", "thisIsATest", "ThisIsATest"},
+		{"Test 5", "parseURL", "ParseURL"},
+		{"Test 6", "getHTTPRequest", "GetHTTPRequest"},
+		{"Test 7", "generateID", "GenerateID"},
+		{"Test 8", "toLowercase", "ToLowercase"},
+		{"Test 9", "convertStringToInt", "ConvertStringToInt"},
+		{"Test 10", "processFile", "ProcessFile"},
+		{"Test 11", "findAndReplace", "FindAndReplace"},
+		{"Test 12", "sortArray", "SortArray"},
+		{"Test 13", "filterData", "FilterData"},
+		{"Test 14", "validateUserInput", "ValidateUserInput"},
+		{"Test 15", "handleError", "HandleError"},
+		{"Test 16", "readConfigFile", "ReadConfigFile"},
+		{"Test 17", "saveToDatabase", "SaveToDatabase"},
+		{"Test 18", "deleteRecord", "DeleteRecord"},
+		{"Test 19", "fetchData", "FetchData"},
+		{"Test 20", "calculateSum", "CalculateSum"},
+		{"Test 21", "openConnection", "OpenConnection"},
+		{"Test 22", "closeConnection", "CloseConnection"},
+		{"Test 23", "parseJSON", "ParseJSON"},
+		{"Test 24", "convertToCamelCase", "ConvertToCamelCase"},
+		{"Test 25", "isPrimeNumber", "IsPrimeNumber"},
+		{"Test 26", "checkPalindrome", "CheckPalindrome"},
+		{"Test 27", "reverseString", "ReverseString"},
+		{"Test 28", "splitSentence", "SplitSentence"},
+		{"Test 29", "mergeArrays", "MergeArrays"},
+		{"Test 30", "capitalizeWords", "CapitalizeWords"},
+		{"Test 31", "encryptPassword", "EncryptPassword"},
+		{"Test 32", "decodeBase64", "DecodeBase64"},
+		{"Test 33", "generateToken", "GenerateToken"},
+		{"Test 34", "validateEmail", "ValidateEmail"},
+		{"Test 35", "processTransaction", "ProcessTransaction"},
+		{"Test 36", "findMaximumValue", "FindMaximumValue"},
+		{"Test 37", "isEvenNumber", "IsEvenNumber"},
+		{"Test 38", "createNewUser", "CreateNewUser"},
+		{"Test 39", "sendHTTPRequest", "SendHTTPRequest"},
+		{"Test 40", "parseCommandLine", "ParseCommandLine"},
+		{"Test 41", "logToConsole", "LogToConsole"},
+		{"Test 42", "convertToSnakeCase", "ConvertToSnakeCase"},
+		{"Test 43", "formatDate", "FormatDate"},
+		{"Test 44", "calculateFactorial", "CalculateFactorial"},
+		{"Test 45", "findDuplicateEntries", "FindDuplicateEntries"},
+		{"Test 46", "sortByColumn", "SortByColumn"},
+		{"Test 47", "getFileExtension", "GetFileExtension"},
+		{"Test 48", "resizeImage", "ResizeImage"},
+		{"Test 49", "uploadFile", "UploadFile"},
+		{"Test 50", "downloadFile", "DownloadFile"},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			// some way to get a result
-			result := "this is an example of a result"
+			result := capitalizeFunctionName(tc.input)
 			if result != tc.expected {
-				t.Errorf("This is an example of an error!")
+				t.Errorf("Error - TestCapitalizeFunctionName: \n\nRESULT\n\n %s\n\nEXPECTED\n\n %s", result, tc.expected)
 			}
 		})
 	}
